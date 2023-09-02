f0cf149e9edaa94358aaead8626b3c27e1591f2e
Thomas Rodrigues <thomas.rodrigues@smarketsolution.com.br>
2023-09-02 02:17:58 -0300

    fix: changing from capitalize to title

--- app/core/utils/string_formater.py
+++ app/core/utils/string_formater.py
@@ -3,4 +3,4 @@ from typing_extensions import Annotated
 from unidecode import unidecode
 
 
-MyStr = Annotated[str, BeforeValidator(lambda v: unidecode(v).capitalize())]
+MyStr = Annotated[str, BeforeValidator(lambda v: unidecode(v).title())]
