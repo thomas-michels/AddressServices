8cc8c0345d16966ad9e293a44ac316e23bb1896e
Thomas Rodrigues <thomas.rodrigues@smarketsolution.com.br>
2023-09-10 20:16:57 -0300

    fix: fixing env

--- app/core/configs/environment.py
+++ app/core/configs/environment.py
@@ -11,24 +11,24 @@ class Environment(BaseSettings):
     """
 
     # APPLICATION
-    APPLICATION_HOST: str
-    APPLICATION_PORT: int
+    APPLICATION_HOST: str = "localhost"
+    APPLICATION_PORT: int = 8000
 
     # DATABASE
-    DATABASE_URL: str
-    DATABASE_HOST: str
-    DATABASE_PORT: int
-    DATABASE_USER: str
-    DATABASE_PASSWORD: str
-    DATABASE_NAME: str
-    ENVIRONMENT: str
-    DATABASE_MIN_CONNECTIONS: int
-    DATABASE_MAX_CONNECTIONS: int
+    DATABASE_URL: str = "localhost:5432"
+    DATABASE_HOST: str = "localhost"
+    DATABASE_PORT: int = 5432
+    DATABASE_USER: str = "user"
+    DATABASE_PASSWORD: str = "password"
+    DATABASE_NAME: str = "test"
+    ENVIRONMENT: str = "test"
+    DATABASE_MIN_CONNECTIONS: int = 1
+    DATABASE_MAX_CONNECTIONS: int = 1
 
     # REDIS
-    REDIS_HOST: str
-    REDIS_PORT: str
-    TIMED_CACHE: int
+    REDIS_HOST: str = "localhost"
+    REDIS_PORT: str = "6379"
+    TIMED_CACHE: int = 30
 
     class Config:
         """Load config file"""
