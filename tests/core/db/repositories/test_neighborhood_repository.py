a13c5470917b1eb026752516da2d6dc2676ff380
Thomas Rodrigues <thomas.rodrigues@smarketsolution.com.br>
2023-09-21 21:30:14 -0300

    fix: fixing tests]

--- tests/core/db/repositories/test_neighborhood_repository.py
+++ tests/core/db/repositories/test_neighborhood_repository.py
@@ -1,7 +1,7 @@
 import pytest
 from unittest.mock import AsyncMock
 from app.core.db.repositories.neighborhood_repository import NeighborhoodRepository
-from app.core.entities import NeighborhoodInDB
+from app.core.entities import NeighborhoodInDB, Neighborhood
 from tests.core.db import mock_connection
 
 @pytest.fixture
@@ -12,12 +12,19 @@ def neighborhood_repository(mock_connection):
 
 @pytest.mark.asyncio
 async def test_insert(neighborhood_repository):
-    neighborhood_repository.conn.execute.return_value = {
+    payload = {
         "id": 1,
         "name": "Neighborhood 1",
+        "population": 123,
+        "houses": 1,
+        "area": 12
     }
 
-    result = await neighborhood_repository.insert("Neighborhood 1")
+    neighborhood_repository.conn.execute.return_value = payload
+
+    neighbor = Neighborhood(**payload)
+
+    result = await neighborhood_repository.insert(neighbor)
 
     assert isinstance(result, NeighborhoodInDB)
     assert result.name == "Neighborhood 1"
@@ -27,6 +34,9 @@ async def test_select_by_id_found(neighborhood_repository):
     neighborhood_repository.conn.execute.return_value = {
         "id": 1,
         "name": "Neighborhood 1",
+        "population": 123,
+        "houses": 1,
+        "area": 12
     }
 
     result = await neighborhood_repository.select_by_id(1)
@@ -47,6 +57,9 @@ async def test_select_by_name_found(neighborhood_repository):
     neighborhood_repository.conn.execute.return_value = {
         "id": 1,
         "name": "Neighborhood 1",
+        "population": 123,
+        "houses": 1,
+        "area": 12
     }
 
     result = await neighborhood_repository.select_by_name("Neighborhood 1")
